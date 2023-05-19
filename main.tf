92a2be7cbe32b4ffb06971e086ab241ee27af767
noeltatah <113138920+noeltatah@users.noreply.github.com>
2023-05-19 12:43:15 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ resource "aws_instance" "DockerInstance" {
   instance_type          = "t2.micro"
   vpc_security_group_ids = ["${ aws_security_group.web-sg.id }"]
   key_name               = aws_key_pair.ec2_key.key_name
-  user_data              = file("install.sh")
+  user_data              = file("${path.module}/install.sh")
  
   tags = {
     Name = "docker instance"
