154b841b05f3db492264e352bbc9f46bfa5f7264
noeltatah <113138920+noeltatah@users.noreply.github.com>
2023-05-19 12:39:50 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
+#!/bin/bash
 # docker install
 sudo yum update -y
-sudo yum install docker
-sudo groupadd docker
+sudo yum install docker -y
 sudo usermod -aG docker $USER
 sudo service docker start
