6290fbf531e1fdb675ebf7a5eb255138d38b2017
kserge2001 <kserge2001@yahoo.fr>
2023-06-29 01:28:36 -0500

    Update main.tf

--- main.tf
+++ main.tf
@@ -77,7 +77,7 @@ data "aws_ami" "amazon-2" {
 resource "aws_instance" "DockerInstance" {
   ami                    = data.aws_ami.amazon-2.id
   instance_type          = "t2.micro"
-  vpc_security_group_ids = ["${ aws_security_group.web-sg.id }"]
+  vpc_security_group_ids = [aws_security_group.web-sg.id]
   key_name               = aws_key_pair.ec2_key.key_name
   user_data              = file("install.sh")
  
